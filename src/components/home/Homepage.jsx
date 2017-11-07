8faa1622c1a298ec0c74e7a82d18d29964b5f019
Mark de Jonge <Mark@Marks-Air.lan>
2017-11-07 20:29:30 +0100

    Bootstrap added

--- src/components/home/Homepage.jsx
+++ src/components/home/Homepage.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import { Button } from 'reactstrap';
 
 import Sidebar from '../shared/Sidebar';
+
+const showAlert = () => {
+  alert('You clicked the button. Well done, Draco!');
+};
 
 export default function HomePage() {
   return (
     <div className="row">
-      <div className="col-sm-12 col-lg-8">
-        This is the home page.
+      <div className="col-sm-12 col-md-8">
+        <p>
+          This is the home page.
+        </p>
+        <p>
+          Here for your enjoyment is a button:
+        </p>
+        <Button onClick={showAlert}>Click Me</Button>
       </div>
       <Sidebar />
     </div>
--- src/components/shared/Sidebar.jsx
+++ src/components/shared/Sidebar.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Card, CardBlock, CardText } from 'reactstrap';
 
 export default function Sidebar() {
   return (
-    <aside className="col-sm-12 col-lg-4">
-      This is the new sidebar component.
+    <aside className="col-sm-12 col-md-4">
+      <Card>
+        <CardBlock>
+          <CardText>
+            Sidebar Item
+          </CardText>
+        </CardBlock>
+      </Card>
     </aside>
   );
 }
