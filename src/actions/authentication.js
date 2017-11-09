94f470f939318427c3dfd08a223e06642b275bef
Mark <mark_de_jonge@live.nl>
2017-11-09 20:27:05 +0100

    added reset password page

--- src/actions/authentication.js
+++ src/actions/authentication.js
@@ -8,6 +8,8 @@ export const loginFailure = error => ({ type: 'AUTHENTICATION_LOGIN_FAILURE', er
 export const loginSuccess = json => ({ type: 'AUTHENTICATION_LOGIN_SUCCESS', json });
 export const logoutFailure = error => ({ type: 'AUTHENTICATION_LOGOUT_FAILURE', error });
 export const logoutSuccess = () => ({ type: 'AUTHENTICATION_LOGOUT_SUCCESS' });
+export const passwordResetHashCreated = () => ({ type: 'AUTHENTICATION_PASSWORD_RESET_HASH_CREATED' });
+export const passwordResetHashFailure = () => ({ type: 'AUTHENTICATION_PASSWORD_RESET_HASH_FAILURE' });
 export const registrationFailure = error => ({ type: 'AUTHENTICATION_REGISTRATION_FAILURE', error });
 export const registrationSuccess = () => ({ type: 'AUTHENTICATION_REGISTRATION_SUCCESS' });
 export const registrationSuccessViewed = () => ({ type: 'AUTHENTICATION_REGISTRATION_SUCCESS_VIEWED' });
@@ -40,6 +42,39 @@ export function checkSession() {
       return dispatch(sessionCheckFailure());
     })
     .catch(error => dispatch(sessionCheckFailure(error)));
+  };
+}
+
+// Send email to API for hashing
+export function createHash(email) {
+  return async (dispatch) => {
+    // contact the API
+    await fetch(
+      // where to contact
+      '/api/authentication/saveresethash',
+      // what to send
+      {
+        method: 'POST',
+        body: JSON.stringify({ email }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        credentials: 'same-origin',
+      },
+    )
+    .then((response) => {
+      if (response.status === 200) {
+        return response.json();
+      }
+      return null;
+    })
+    .then((json) => {
+      if (json.username) {
+        return dispatch(passwordResetHashCreated(json));
+      }
+      return dispatch(passwordResetHashFailure(new Error('Something went wrong. Please try again.')));
+    })
+    .catch(error => dispatch(passwordResetHashFailure(error)));
   };
 }
 
--- src/components/Template.jsx
+++ src/components/Template.jsx
@@ -8,6 +8,7 @@ import LoginPage from './account/LoginPageContainer';
 import ProfilePage from './account/ProfilePage';
 import RegisterPage from './account/RegisterPageContainer';
 import RegistrationSuccessPage from './account/RegistrationSuccessPageContainer';
+import ResetPasswordPage from './account/ResetPasswordPageContainer';
 
 export default function Template(props) {
   const { authentication, progress } = props;
@@ -21,6 +22,7 @@ export default function Template(props) {
           <Route exact path="/account/login" component={LoginPage} />
           <Route exact path="/account/register" component={RegisterPage} />
           <Route exact path="/account/registration-success" component={RegistrationSuccessPage} />
+          <Route exact path="/account/reset-password" component={ResetPasswordPage} />
           <Route path="/account/profile/:id" component={ProfilePage} />
         </section>
         <div className="loader-wrapper" style={progress > 0 ? { display: 'block' } : { display: 'none' }}>
